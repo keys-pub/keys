f3f1d54a3cd3bd48c28c7253589480706e514240
Gabriel Handford <gabrielh@gmail.com>
2020-04-14 08:53:40 -0700

    Update item_test.go

--- keyring/item_test.go
+++ keyring/item_test.go
@@ -32,11 +32,11 @@ func TestItem(t *testing.T) {
 
 	b, err = msgpack.Marshal(item)
 	require.NoError(t, err)
-	expected := `([]uint8) (len=55 cap=64) {
+	expected := `([]uint8) (len=56 cap=64) {
  00000000  84 a2 69 64 a8 61 63 63  6f 75 6e 74 31 a3 74 79  |..id.account1.ty|
  00000010  70 aa 70 61 73 73 70 68  72 61 73 65 a3 64 61 74  |p.passphrase.dat|
- 00000020  c4 08 70 61 73 73 77 6f  72 64 a2 74 73 d7 ff 00  |..password.ts...|
- 00000030  3d 09 00 49 96 02 d2                              |=..I...|
+ 00000020  c4 08 70 61 73 73 77 6f  72 64 a3 63 74 73 d7 ff  |..password.cts..|
+ 00000030  00 3d 09 00 49 96 02 d2                           |.=..I...|
 }
 `
 	require.Equal(t, expected, spew.Sdump(b))
