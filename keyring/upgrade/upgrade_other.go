3579aa95e09154944fab2c3a1db3814920063e63
Gabriel Handford <gabrielh@gmail.com>
2020-04-17 15:40:12 -0700

    keyring: Remove upgrade

--- keyring/upgrade/upgrade_other.go
+++
@@ -1,8 +0,0 @@
-// +build !windows,!darwin
-
-package upgrade
-
-func KeyringV1(serviceFrom string, serviceTo string, password string) {
-	logger.Infof("Keyring upgrade not supported")
-	// Not supported
-}
