a94d2050e2ae5e192a2f7cc6f3743a894dae5ba5
Gabriel Handford <gabrielh@gmail.com>
2020-05-17 16:29:41 -0700

    Update encrypt_examples_test.go

--- saltpack/encrypt_examples_test.go
+++ saltpack/encrypt_examples_test.go
@@ -3,7 +3,6 @@
 package saltpack_test
 
 import (
-	"encoding/hex"
 	"fmt"
 	"log"
 
@@ -29,20 +28,23 @@ func ExampleEncryptArmored() {
 func ExampleDecryptArmored() {
 	// Message from ExampleEncryptArmored
 	aliceID := keys.ID("kbx17jhqvdrgdyruyseuaat0rerj7ep4n63n4klufzxtzmk9p3d944gs4fg39g")
-	encrypted := `BEGIN SALTPACK ENCRYPTED MESSAGE. 
-	kcJn5brvybfNjz6 D5ll2Nk0YiiGFCz I2xgcLXuPzYNBe3 OboFDA8Gh0TxosH yo82IRf2OZzteqO 
-	GaPWlm7t0lC0M0U vNnOvsussLf1nMw Oo1Llf9oAbA7qxa GjupUEWnTgyjjUn GKb3WvtjSgRsJS2 
-	Y92GMEx8cHvbGrJ zvLGlbqAhEIDNZ2 SE15aoV6ahVxeVH 1inHyghv3H1oTAC K86mBl4fg9FY1QK 
+	encrypted := `BEGIN SALTPACK ENCRYPTED MESSAGE.
+	kcJn5brvybfNjz6 D5ll2Nk0YiiGFCz I2xgcLXuPzYNBe3 OboFDA8Gh0TxosH yo82IRf2OZzteqO
+	GaPWlm7t0lC0M0U vNnOvsussLf1nMw Oo1Llf9oAbA7qxa GjupUEWnTgyjjUn GKb3WvtjSgRsJS2
+	Y92GMEx8cHvbGrJ zvLGlbqAhEIDNZ2 SE15aoV6ahVxeVH 1inHyghv3H1oTAC K86mBl4fg9FY1QK
 	4n0gLOmSHbD8UYH V3HAPS0yaBC4xJB g3y04Xcqiij36Nb WmJgvSFdGugXd7O yfU.
 	END SALTPACK ENCRYPTED MESSAGE.
 	`
 
 	// bobID := keys.ID("kbx18x22l7nemmxcj76f9l3aaflc5487lp5u5q778gpe3t3wzhlqvu8qxa9z07")
-	b, err := hex.DecodeString("7f163f9cd538797c7c5ba5abf4f4acdfa5f52f2ffe753c40bd12f87ce279df1b")
+	key := `BEGIN X25519 KEY MESSAGE.
+	umCRo9iHIudLWoz 4Ugt0hUXQVJ7lhV p7A9mb3kOTg6PeV fhqetAc9ZOUjagi
+	91gENEkp0xfjF2E Tyakwe90kzo1FNT gRacWRL5B59strN OoZYHQooqvlMKM.
+	END X25519 KEY MESSAGE.`
+	bob, err := keys.DecodeKeyFromSaltpack(key, "", true)
 	if err != nil {
 		log.Fatal(err)
 	}
-	bob := keys.NewX25519KeyFromPrivateKey(keys.Bytes32(b))
 
 	// Bob decrypts
 	out, sender, err := saltpack.DecryptArmored(encrypted, saltpack.NewKeyStore(bob))
