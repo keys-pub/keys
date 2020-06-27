3c86b2449314464ea931a0146c1d9cd096520f6d
Gabriel Handford <gabrielh@gmail.com>
2020-06-27 12:42:27 -0700

    request: Allow simple redirects (#94)

--- request/request_test.go
+++ request/request_test.go
@@ -9,10 +9,40 @@ import (
 )
 
 func TestReddit(t *testing.T) {
-	// t.Skip()
 	req := request.NewHTTPRequestor()
-
-	urs := "https://reddit.com/r/keyspubmsgs/comments/f8g9vd/gabrlh.json"
-	_, err := req.RequestURLString(context.TODO(), urs)
+	urs := "https://www.reddit.com/r/keyspubmsgs/comments/f8g9vd/gabrlh.json"
+	res, err := req.RequestURLString(context.TODO(), urs)
 	require.NoError(t, err)
+	require.NotEmpty(t, res)
+}
+
+func TestTwitter(t *testing.T) {
+	req := request.NewHTTPRequestor()
+	urs := "https://mobile.twitter.com/gabrlh/status/1222706272849391616"
+	res, err := req.RequestURLString(context.TODO(), urs)
+	require.NoError(t, err)
+	require.NotEmpty(t, res)
+}
+
+func TestTwitterRedirect(t *testing.T) {
+	req := request.NewHTTPRequestor()
+	// Redirect (from lowercase) to /Boboloblaws/status/1276948233915207680 is ok
+	urs := "https://mobile.twitter.com/boboloblaws/status/1276948233915207680"
+	res, err := req.RequestURLString(context.TODO(), urs)
+	require.NoError(t, err)
+	require.NotEmpty(t, res)
+
+	// Tweet ID from different user, should not redirect
+	urs = "https://mobile.twitter.com/boboloblaws/status/1222706272849391616"
+	res, err = req.RequestURLString(context.TODO(), urs)
+	require.NoError(t, err)
+	require.Empty(t, res)
+}
+
+func TestGithub(t *testing.T) {
+	req := request.NewHTTPRequestor()
+	urs := "https://gist.github.com/gabriel/ceea0f3b675bac03425472692273cf52"
+	res, err := req.RequestURLString(context.TODO(), urs)
+	require.NoError(t, err)
+	require.NotEmpty(t, res)
 }
