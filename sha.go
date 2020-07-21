83da55f85b1b3c0ce079f0bb280a5a84e1e30440
Gabriel Handford <gabrielh@gmail.com>
2020-07-21 13:29:02 -0700

    -SHA256 (#106)
    Use sha256.Sum256 instead

--- sha.go
+++ sha.go
@@ -5,22 +5,6 @@ import (
 	"crypto/sha256"
 )
 
-// SHA256 for bytes.
-func SHA256(b []byte) []byte {
-	if len(b) == 0 {
-		panic("empty bytes")
-	}
-	h := sha256.New()
-	if _, err := h.Write(b); err != nil {
-		panic(err)
-	}
-	bs := h.Sum(nil)
-	if len(bs) == 0 {
-		panic("empty bytes for sum")
-	}
-	return bs
-}
-
 // HMACSHA256 does a HMAC-SHA256 on msg with key.
 func HMACSHA256(key []byte, msg []byte) []byte {
 	if len(key) == 0 {
