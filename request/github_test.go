25148d1a7f40404ad4a37701cf241c30f97335dd
Gabriel Handford <gabrielh@gmail.com>
2020-09-02 16:08:00 -0700

    request: Fix reddit test (#134)

---
+++ request/github_test.go
@@ -0,0 +1,21 @@
+package request_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/keys-pub/keys/encoding"
+	"github.com/keys-pub/keys/request"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGithub(t *testing.T) {
+	req := request.NewHTTPRequestor()
+	urs := "https://gist.github.com/gabriel/ceea0f3b675bac03425472692273cf52"
+	res, err := req.RequestURLString(context.TODO(), urs)
+	require.NoError(t, err)
+
+	out, brand := encoding.FindSaltpack(string(res), true)
+	require.Equal(t, "kdZaJI1U5AS7G6iVoUxdP8OtPzEoM6pYhVl0YQZJnotVEwLg9BDb5SUO05pmabUSeCvBfdPoRpPJ8wrcF5PP3wTCKq6Xr2MZHgg6m2QalgJCD6vMqlBQfIg6QsfB27aP5DMuXlJAUVIAvMDHIoptmSriNMzfpwBjRShVLWH70a0GOEqD6L8bkC5EFOwCedvHFpcAQVqULHjcSpeCfZEIOaQ2IP", out)
+	require.Equal(t, "", brand)
+}
