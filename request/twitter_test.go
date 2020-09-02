a41e736807d5c6793b89ead4ff83bb567b809550
Gabriel Handford <gabrielh@gmail.com>
2020-09-02 16:05:29 -0700

    More testing

--- request/twitter_test.go
+++ request/twitter_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"testing"
 
+	"github.com/keys-pub/keys/encoding"
 	"github.com/keys-pub/keys/request"
 	"github.com/stretchr/testify/require"
 )
@@ -13,7 +14,10 @@ func TestTwitter(t *testing.T) {
 	urs := "https://mobile.twitter.com/gabrlh/status/1222706272849391616"
 	res, err := req.RequestURLString(context.TODO(), urs)
 	require.NoError(t, err)
-	require.NotEmpty(t, res)
+
+	out, brand := encoding.FindSaltpack(string(res), true)
+	require.Equal(t, "EqcgDt8RfXvPq9b4qCV8S3VPKIQKqaN7Rc1YruQQYuVS8niHzUv7jdykkEPSrKGcJQCNTkNE7uFswPuwfpaZX6TCKq6Xr2MZHgg6S0MjgWFMJ1KHxazTuXs4icK3k8SZCR8mVLQMSVhFeMrvz0qJOmA96zW9RAY6whsLo5fC8i3fRJjyo9mQJZid8MwBXJl1XDL5ZOSkLYs6sk6a2gCiGyA2IP", out)
+	require.Equal(t, "", brand)
 }
 
 func TestTwitterRedirect(t *testing.T) {
@@ -22,19 +26,13 @@ func TestTwitterRedirect(t *testing.T) {
 	urs := "https://mobile.twitter.com/boboloblaws/status/1276948233915207680"
 	res, err := req.RequestURLString(context.TODO(), urs)
 	require.NoError(t, err)
-	require.NotEmpty(t, res)
+	out, brand := encoding.FindSaltpack(string(res), true)
+	require.Equal(t, "fOT1ZDzPbT8yCx3eTu16VZBkcU2YuYnl5YGxpbOvSEMOc84RdH5misz4SOFDPsDKTMCerICVBF2RXcSmvRfjCZTCKq6Xr2MZHgg4N95u0vFmKKWAQGem19QxSORCCZNXsRq8v6WKe2SvFy1odL0Xm2VIeNebn4c9xjJwYTBKNKRtSmijNnmAqBjs61S48Vwrjlx67QFSowDMw9jZhAf0i992BwmNTeOh3", out)
+	require.Equal(t, "", brand)
 
 	// Tweet ID from different user, should not redirect
 	urs = "https://mobile.twitter.com/boboloblaws/status/1222706272849391616"
 	res, err = req.RequestURLString(context.TODO(), urs)
 	require.NoError(t, err)
 	require.Empty(t, res)
-}
-
-func TestGithub(t *testing.T) {
-	req := request.NewHTTPRequestor()
-	urs := "https://gist.github.com/gabriel/ceea0f3b675bac03425472692273cf52"
-	res, err := req.RequestURLString(context.TODO(), urs)
-	require.NoError(t, err)
-	require.NotEmpty(t, res)
 }
