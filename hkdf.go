a2f611522be48c8d1fae6f9fc6beddf2002eff0e
Gabriel Handford <gabrielh@gmail.com>
2021-02-01 17:03:30 -0800

    Linting

--- hkdf.go
+++ hkdf.go
@@ -7,7 +7,7 @@ import (
 	"golang.org/x/crypto/hkdf"
 )
 
-// HKDFSHA256 expands a secret into another secret using HKDF with SHA256.
+// HKDFSHA256 uses HKDF with SHA256.
 // The `len` for output byte length.
 // The `salt` is non-secret salt, optional (can be nil), recommended: hash-length random value.
 // The `info` is non-secret context info, optional (can be empty).
